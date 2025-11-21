59f72baa9b0bf1c39275aa281f1c8202e3c055c6
MagnusTrier <magnustrier@gmail.com>
2025-11-21 14:16:09 +0100

    added delimiter count exception

--- src/functions.py
+++ src/functions.py
@@ -22,3 +22,13 @@ def text_node_to_html_node(text_node):
             })
         case _:
             raise Exception('Invalid TextNode text_type')
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type is not TextType.TEXT:
+            new_nodes.append(old_node)
+        else:
+            if old_node.text.count(delimiter) % 2 != 0:
+                raise Exception(f'Old node has invalid markdown syntax unmatched delimiter: {delimiter}')
+
